ad88ec92a3104c2135b5dba6aee7490c98d05854
Christiaan Dotze <christiaan@dotze.co.za>
2020-04-08 09:52:08 +0200

    modified files

--- src/main/java/za/co/dotze/greeting/GreetingController.java
+++ src/main/java/za/co/dotze/greeting/GreetingController.java
@@ -19,7 +19,7 @@ public class GreetingController {
 
     @RequestMapping(value = "/", method = GET)
     public String defaultPage() {
-        return "<a href=https://gentle-waters-91664.herokuapp.com/greeting?name=Christiaan>Click Here</a>";
+        return "<a href=https://gentle-waters-91664.herokuapp.com/greeting?name=Christiaan>Click Here!</a>";
     }
 
     @RequestMapping(value = "/greeting", method = GET)
